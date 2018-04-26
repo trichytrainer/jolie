3cef8752304336077bf46cbb412ea44275199a21
Filip Weidemann <filip.weidemann@outlook.de>
2018-04-26 15:55:26 +0200

    docker-spawner now class based, accessing web app route spawns new container

--- backend/api.py
+++ backend/api.py
@@ -1,12 +1,20 @@
 from flask import Flask
 from flask_restful import reqparse, abort, Api, Resource
+from docker_spawner import DockerClient
 
 app = Flask(__name__)
 api = Api(app)
 
 class Home(Resource):
     def get(self):
-        return 'Works!'
+        c = DockerClient()
+        container = c.spawn_container(
+            image='python:3.7-rc-stretch',
+            command="sh -c 'pip3 install pytest && cd code && python testrunner.py'",
+            detach=True
+        )
+        logs = c.get_container_logs(container)
+        return logs
 
 api.add_resource(Home, '/home')
 
--- backend/docker_spawner.py
+++ backend/docker_spawner.py
@@ -2,11 +2,54 @@ import os
 import time
 import docker
 
-client = docker.from_env()
-api_client = docker.APIClient()
+class DockerClient:
+    def __init__(self):
+        self.client = docker.from_env()
+        self.current_path = os.path.dirname(os.path.realpath(__file__))
+        self.volumes = {
+            self.current_path: {
+                'bind': '/code',
+                'mode': 'rw'
+            }
+        }
 
-image = "python:3.7-rc-stretch"
+    def set_volumes(self, volume_definition):
+        self.volumes = volume_definition
+
+    def spawn_container(self, image, command, volumes=None, detach=False):
+        return self.client.containers.run(
+            image=image,
+            command=command,
+            volumes=self.volumes,
+            detach=detach
+        )
+
+    def get_container_logs(self, container):
+        stderr = ''
+        stdout = ''
+
+        for line in container.logs(stream=True, stderr=True, stdout=False):
+            stderr += line.decode('utf-8')
+
+        for line in container.logs(stream=True, stderr=False, stdout=True):
+            stdout += line.decode('utf-8')
+
+        return {
+            'stderr': stderr,
+            'stdout': stdout
+        }
+
+    def stop_container(self, container):
+        container.stop()
+
+    def clear_containers(self):
+        return self.client.containers.prune()
+
+
+"""
+client = docker.from_env()
 current_path = os.path.dirname(os.path.realpath(__file__))
+image = "python:3.7-rc-stretch"
 
 volumes = {
     current_path: {
@@ -38,3 +81,4 @@ print('standarderr: ' + stderr)
 
 container.stop()
 deleted_containers = client.containers.prune()
+"""
